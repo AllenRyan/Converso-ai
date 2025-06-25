d83d7716b551bfdd0451d38f6cd868ac20738a3a
Abrar Khan <abrarisop786@gmail.com>
2025-06-25 08:19:07 +0500

    implement clerk auth & pricing and supabase

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import NavItems from "./NavItems";
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+  } from '@clerk/nextjs'
 
 const Navbar = () => {
     return ( 
@@ -15,9 +21,16 @@ const Navbar = () => {
                     />
                 </div>
             </Link>
-            <div className="flex items-center gap-8">
+            <div className="flex items-center gap-6">
                 <NavItems />
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/" />
+                </SignedIn>
             </div>
         </nav>
      );
